d8c431d91cd16af3ef0db4f8af19eca8e92b56a7
Temesgen Adane <tedenadane@gmail.com>
2025-06-21 06:46:59 -0700

    start using scss fully

--- assets/javascript/menu.js
+++ assets/javascript/menu.js
@@ -1,14 +1,14 @@
-const openBtn = document.querySelector(".menu-icon");
-const closeBtn = document.querySelector(".close-icon");
-const menu = document.querySelector(".menu");
-const menuOverlay = document.querySelector(".menu-overlay");
+const openBtn = document.querySelector(".header__menu-open-icon");
+const closeBtn = document.querySelector(".header-menu__close-icon");
+const menu = document.querySelector(".header-menu");
+const menuContent = document.querySelector(".header-menu__content");
 
 openBtn.addEventListener("click", () => {
-  menu.classList.add("show");
-  menuOverlay.style.display = "block";
+  menu.classList.remove("is-hidden");
+  menuContent.classList.add("show");
 });
 
 closeBtn.addEventListener("click", () => {
-  menu.classList.remove("show");
-  menuOverlay.classList.add("is-hidden");
+  menu.classList.add("is-hidden");
+  menuContent.classList.remove("show");
 });
